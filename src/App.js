ef3910d0867d6b728061df853866702afcf5b122
erlan1405 <erlander5551@gmail.com>
2024-12-09 14:49:49 -0500

    create react app

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import {useState, useEffect} from "react";
+import "./App.css";
+
+import MovieDisplay from "./MovieDisplay";
+import Form from "./Form";
+
+export default function App() {
+  const apiKey = "61afb62e";
+
+  const [movie, setMovie] = useState(null);
+
+  const getMovie = async (searchTerm) => {
+    const response = await fetch(
+      `http://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`
+    );
+    const data = await response.json();
+    setMovie(data);
+  };
+
+  // This will run on the first render but not on subsquent renders
+  useEffect(() => {
+    getMovie("Clueless");
+  }, []);
+
+  return (
+    <div className="App">
+      <Form moviesearch={getMovie} />
+      <MovieDisplay movie={movie} />
+    </div>
+  );
+}
---
+++ src/MovieDisplay.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function MovieDisplay({ movie }) {
+
+    const loaded = () => (
+        <>
+            <h1>{movie.Title}</h1>
+            <h2>{movie.Genre}</h2>
+            <img src={movie.Poster} alt={movie.Title} />
+            <h2>{movie.Year}</h2>
+        </>
+    );
+
+    const loading = () => <h2>Loading...</h2>;
+
+    return movie ? loaded() : loading();
+}
